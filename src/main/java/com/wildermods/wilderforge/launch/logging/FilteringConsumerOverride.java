831eca7c9dbe66e4289055c79c12af3bed9b0ea8
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-08-11 16:53:59 -0400

    Game sysouts are now redirected to Log4j loggers (Progress on #2)

---
+++ src/main/java/com/wildermods/wilderforge/launch/logging/FilteringConsumerOverride.java
@@ -0,0 +1,59 @@
+package com.wildermods.wilderforge.launch.logging;
+
+import java.util.LinkedHashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.MessageFormatMessageFactory;
+import org.apache.logging.log4j.Level;
+import static org.apache.logging.log4j.Level.*;
+
+import com.worldwalkergames.logging.FilteringConsumer;
+
+public class FilteringConsumerOverride extends FilteringConsumer {
+	
+	public static final LinkedHashMap<String, Logger> LOGGERS = new LinkedHashMap<String, Logger>();
+	
+	public FilteringConsumerOverride(Filter... filters) {
+		super(filters);
+	}
+	
+	@Override
+	protected void log(String[] tags, int level, long time, String message, Object... parameters) {
+		String tag = tags[0];
+		if(tag == null) {
+			tag = "UNKNOWN-WILDERFORGE";
+		}
+		getLogger(tag).log(getLevel(level), message, parameters);
+	}
+	
+	private Logger getLogger(String name) { 
+		Logger ret;
+		if((ret = LOGGERS.get(name)) != null) {
+			return ret;
+		}
+		ret = LogManager.getLogger(name, new MessageFormatMessageFactory());
+		LOGGERS.putIfAbsent(name, ret);
+		return ret;
+	}
+	
+	private final Level getLevel(int level) {
+		switch (level) {
+		case 0:
+			return TRACE;
+		case 1:
+			return DEBUG;
+		case 2:
+			return INFO;
+		case 3:
+			return WARN;
+		case 4:
+			return ERROR;
+		case 5:
+			return FATAL;
+		default:
+			return INFO;
+		}
+	}
+	
+}
---
+++ src/main/java/com/wildermods/wilderforge/mixins/LegacyDesktopMixin.java
@@ -0,0 +1,23 @@
+package com.wildermods.wilderforge.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.wildermods.wilderforge.launch.logging.FilteringConsumerOverride;
+import com.worldwalkergames.legacy.LegacyDesktop;
+import com.worldwalkergames.logging.ALogger;
+import com.worldwalkergames.logging.FilteringConsumer;
+
+@Mixin(value = LegacyDesktop.class, remap = false)
+public class LegacyDesktopMixin {
+
+	@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/LinkedList;add(Ljava/lang/Object;)Z"), method = "initializeLogging()V")
+	private static void initializeLogging(CallbackInfo c) {
+		ALogger.Aggregator aggregator = ALogger.getDefaultAggregator();
+		aggregator.consumers.clear();
+		aggregator.consumers.add(new FilteringConsumerOverride(new FilteringConsumer.Filter()));
+	}
+	
+}
