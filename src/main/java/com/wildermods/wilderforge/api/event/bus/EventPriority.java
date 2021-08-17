8073672f5671c4a746b86da4cebf91aa48df892a
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-08-17 10:58:47 -0400

    Add EventPriority

---
+++ src/main/java/com/wildermods/wilderforge/api/event/bus/EventPriority.java
@@ -0,0 +1,11 @@
+package com.wildermods.wilderforge.api.event.bus;
+
+public interface EventPriority {
+
+	public static final int LOWER = -10000;
+	public static final int LOW = -5000;
+	public static final int NORMAL = 0;
+	public static final int HIGH = 5000;
+	public static final int HIGHER = 10000;
+	
+}
