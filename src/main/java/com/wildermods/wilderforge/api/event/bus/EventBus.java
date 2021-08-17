7afef8c1b37d00bfe04d9cac53649bf02374052b
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-08-16 20:19:37 -0400

    Document EventBus

--- src/main/java/com/wildermods/wilderforge/api/event/bus/EventBus.java
+++ src/main/java/com/wildermods/wilderforge/api/event/bus/EventBus.java
@@ -24,12 +24,65 @@ public class EventBus {
 		
 	}
 	
+	/**
+	 * Registers a class to receive events.
+	 * <p>
+	 * All <code>static</code> <code>{@link com.wildermods.wilderforge.api.event.bus.SubscribeEvent @SubscribeEvent}</code> methods become
+	 * eligible to receive their events.
+	 * <p>
+	 * Unlike standard objects, Once registered, classes are always eligible to receive events, and they will not become
+	 * eligible to be garbage collected. EventBus will keep a strong reference to the Class instance to prevent garbage collection.
+	 * 
+	 */
+	@SuppressWarnings("rawtypes")
+	public final void register(Class c) {
+		removeCollectedReferences();
+		registerListeners(c);
+	}
+	
+	/**
+	 * Registers an object instance to receive events.
+	 * <p>
+	 * All non-<code>static</code> <code>{@link com.wildermods.wilderforge.api.event.bus.SubscribeEvent @SubscribeEvent}</code> methods become
+	 * eligible to receive their events.
+	 * <p>
+	 * Note that EventBus will only keep {@link java.lang.ref.WeakReference weak references} to your object in order to prevent memory leaks. This means you 
+	 * must keep a strong reference to your object until the object no longer needs to receive events, otherwise your object may be garbage collected, and
+	 * will not receive events.
+	 * <p>For example, suppose <code>Foo</code> instances are eligible to receive <code>BarEvent</code> events:
+	 * <pre>
+	 * <code>
+	 * registerSubscribers() {
+	 *     EVENT_BUS.register(new Foo());
+	 *     Foo f = new Foo();
+	 *     Thread.sleep(1000);
+	 *     f = null;
+	 *     Thread.sleep(1000);
+	 *     EVENT_BUS.fire(new BarEvent());
+	 * }
+	 * </code>
+	 * </pre>
+	 * <p>Both the <code>new Foo()</code> and <code>f</code> instances might not receive the event because there is not a strong reference to them when
+	 * <code>EVENT_BUS.fire</code> is called.
+	 * <p><b>Implementation detail:</b> passing an instance of <code>Class</code>
+	 * I.E. Calling 
+	 * <p>
+	 * <code>register((Object)Example.class);</code>
+	 * <p>
+	 * will not provide the behavior stated above. This method will
+	 * instead behave as if {@link #register(Class)} was called
+	 */
 	public final void register(Object o) {
 		removeCollectedReferences();
 		registerListeners(o);
 	}
 	
 	/**
+	 * <p>Fires an event to all subscribers that haven't been garbage collected, in {@link conm.wildermods.wilderforge.api.event.bus.SubScribeEvent#priority() priority order}.
+	 * 
+	 * <p>Note that it is still possible for subscribers which have no strong
+	 * references to receive the event.
+	 * 
 	 * @param e event
 	 * @return true if the event was cancelled, false otherwise
 	 */
