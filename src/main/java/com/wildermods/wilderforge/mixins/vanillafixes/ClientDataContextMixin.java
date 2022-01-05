91d50059b9a911fbc197736e2aa3e84018d7d56f
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-01-05 18:30:02 -0500

    Move vanilla bugfix mixins to their own package.

--- src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/ClientDataContextMixin.java
+++ src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/ClientDataContextMixin.java
@@ -1,4 +1,4 @@
-package com.wildermods.wilderforge.mixins;
+package com.wildermods.wilderforge.mixins.vanillafixes;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
