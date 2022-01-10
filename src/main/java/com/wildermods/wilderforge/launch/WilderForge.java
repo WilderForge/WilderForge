cc01a26d660553dec707f6acdbf9f8d4f7b4e00a
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-01-10 06:16:33 -0500

    rm debug code

--- src/main/java/com/wildermods/wilderforge/launch/WilderForge.java
+++ src/main/java/com/wildermods/wilderforge/launch/WilderForge.java
@@ -4,9 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.wildermods.wilderforge.api.eventV1.bus.EventBus;
-import com.wildermods.wilderforge.api.eventV1.bus.SubscribeEvent;
-import com.wildermods.wilderforge.api.overlandV1.party.PartyCreateEvent;
-import com.wildermods.wilderforge.api.overlandV1.party.PartyDisbandEvent;
 import com.worldwalkergames.legacy.context.LegacyViewDependencies;
 
 public final class WilderForge {
@@ -36,25 +33,6 @@ public final class WilderForge {
 			throw new IllegalStateException();
 		}
 		EVENT_BUS.register(WilderForge.class);
-	}
-	
-	@SubscribeEvent
-	public static void onPartyCreate(PartyCreateEvent.Post e) {
-		System.out.println("A party of size " + e.getPartyProposal().members.size + " was created!");
-	}
-	
-	@SubscribeEvent
-	public static void onPartyDisband(PartyDisbandEvent e) {
-		int size = e.getParty().members.size();
-		System.out.println("Game wants to disband party of size " + size);
-		if(size == 2 && e.canBeCancelled()) {
-			e.setCancelled(true);
-			new Throwable().printStackTrace();
-			System.out.println("Cancelling disbandonment of party with size " + size);
-		}
-		else {
-			System.out.println("Allowing disbandonment of party with size " + size);
-		}
 	}
 	
 	public static LegacyViewDependencies getViewDependencies() {
