6c6d883a1915c53bf6478f829bf8d192d6315160
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2024-09-03 21:46:40 -0400

    Fix anyKeyDown signal processor complaining of too many listeners

---
+++ src/main/java/com/wildermods/wilderforge/mixins/input/GlobalInputProcessorMixin.java
@@ -0,0 +1,37 @@
+package com.wildermods.wilderforge.mixins.input;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.wildermods.wilderforge.api.mixins.v1.Cast;
+import com.wildermods.wilderforge.launch.WilderForge;
+import com.wildermods.wilderforge.launch.logging.LogLevel;
+import com.worldwalkergames.communication.observer.signals.Signal2;
+import com.worldwalkergames.legacy.input.GlobalInputProcessor;
+
+@Mixin(GlobalInputProcessor.class)
+public class GlobalInputProcessorMixin {
+	
+	public @Shadow Signal2<GlobalInputProcessor, Object> anyKeyDown;
+	
+	@Inject(
+		method = "<init>("
+					+ "Lcom/worldwalkergames/legacy/context/CursorManager;"
+					+ "Lcom/worldwalkergames/legacy/options/InterfaceOptions;"
+					+ "Lcom/worldwalkergames/legacy/options/Keymap;"
+				+ ")V",
+		at = @At (
+			value = "TAIL"
+		),
+		require = 1
+	)
+	private void unlimitedAnyInputListener(CallbackInfo c) {
+		final int capacity = -1;
+		WilderForge.LOGGER.log(LogLevel.DEBUG, "set anyKeyDown signal capacity to " + capacity, getClass().getSimpleName());
+		anyKeyDown = new Signal2<>(Cast.from(this), capacity, false);
+	}
+	
+}
