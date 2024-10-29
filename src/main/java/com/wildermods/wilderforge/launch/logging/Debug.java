959ef514dc6b92c4a4c302dcbe041fff3df81f07
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2024-10-29 19:36:57 -0400

    Add debugging code

---
+++ src/main/java/com/wildermods/wilderforge/launch/logging/Debug.java
@@ -0,0 +1,25 @@
+package com.wildermods.wilderforge.launch.logging;
+
+import com.wildermods.wilderforge.launch.WilderForge;
+
+public class Debug extends Error {
+
+	private Debug(String reason) {
+		super(reason);
+	}
+	
+	public static void trace() {
+		trace(null);
+	}
+	
+	public static void trace(String reason) {
+		if(reason == null || reason.isBlank()) {
+			reason = "Tracing code path";
+		}
+		else {
+			reason = "Tracing code path - " + reason;
+		}
+		WilderForge.LOGGER.catching(LogLevel.INFO, new Debug("Tracing code path "));
+	}
+	
+}
