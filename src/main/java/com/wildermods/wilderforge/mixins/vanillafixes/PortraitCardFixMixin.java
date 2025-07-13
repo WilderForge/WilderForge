af60c448d2087d5a0462b34be4dba2570666a101
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2025-07-13 07:50:17 -0400

    do not use the same method name as what is in the target class

--- src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/PortraitCardFixMixin.java
+++ src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/PortraitCardFixMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -32,7 +33,7 @@ public class PortraitCardFixMixin {
 				+ "Lcom/worldwalkergames/legacy/game/mission/model/MissionMap;"
 		)
 	)
-	public MissionMap update(EntitiesCollection entities, Operation<MissionMap> original) {
+	private @Unique MissionMap fixInjuryValueWhenMapIsNull(EntitiesCollection entities, Operation<MissionMap> original) {
 		MissionMap map = original.call(entities);
 		if(map == null) {
 			healthBarRenderer.injurySnapshots.put(this.entityId, 0);
