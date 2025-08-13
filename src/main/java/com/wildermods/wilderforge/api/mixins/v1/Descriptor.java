3fd73974fdc6660a32150f68ec1ec9175553b5ab
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2025-08-13 14:58:32 -0400

    add enum values

--- src/main/java/com/wildermods/wilderforge/api/mixins/v1/Descriptor.java
+++ src/main/java/com/wildermods/wilderforge/api/mixins/v1/Descriptor.java
@@ -1,6 +1,7 @@
 package com.wildermods.wilderforge.api.mixins.v1;
 
 public class Descriptor {
+	//Primitive Types
 	public static final String BYTE = "B";
 	public static final String CHAR = "C";
 	public static final String DOUBLE = "D";
@@ -12,8 +13,11 @@ public class Descriptor {
 	public static final String VOID = "V";
 	public static final String ARRAY_OF = "[";
 	
+	//methods
 	public static final String STRING = "Ljava/lang/String;";
 	public static final String OBJECT = "Ljava/lang/Object;";
 	public static final String GDX_ARRAY = "Lcom/badlogic/gdx/utils/Array;";
 
+	//fields
+	public static final String ENUM_VALUES = "$VALUES";
 }
