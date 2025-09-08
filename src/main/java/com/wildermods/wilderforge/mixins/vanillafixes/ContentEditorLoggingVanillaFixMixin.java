c971a4635e6d4c8f481eb9a10b24392a0684a40f
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2025-09-07 21:10:12 -0400

    [Vanilla] Fix content editor save failures not being logged correctly

---
+++ src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/ContentEditorLoggingVanillaFixMixin.java
@@ -0,0 +1,40 @@
+package com.wildermods.wilderforge.mixins.vanillafixes;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.wildermods.wilderforge.api.mixins.v1.Descriptor;
+import com.worldwalkergames.legacy.editor.ContentEditor;
+import com.worldwalkergames.logging.ALogger;
+
+@Mixin(ContentEditor.class)
+public class ContentEditorLoggingVanillaFixMixin {
+
+	@WrapOperation(
+		method = "saveAll",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/worldwalkergames/logging/ALogger;"
+					+ "log4("
+						+ Descriptor.STRING
+						+ Descriptor.ARRAY_OF + Descriptor.OBJECT
+					+ ")" + Descriptor.VOID
+		)
+	)
+	public void fixIncorrectLogging(ALogger instance, String message, Object[] args, Operation<Void> original, @Local RuntimeException caught) {
+		if(args == null) {
+			args = new Object[1];
+		}
+		if(args.length == 0) {
+			args = new Object[1];
+		}
+		if(args[0] == null) {
+			args[0] = caught;
+		}
+		original.call(instance, message, args);
+	}
+	
+}
