614f2ddbcb2be14eded358364f0547a71fa377f2
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2025-09-15 14:56:28 -0400

    Fix Katakana not being seen as unbreakable text

---
+++ src/main/java/com/wildermods/wilderforge/mixins/vanillafixes/KatakanaJapaneseTextFixMixin.java
@@ -0,0 +1,20 @@
+package com.wildermods.wilderforge.mixins.vanillafixes;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import com.badlogic.gdx.graphics.g2d.freetype.NiceFreeTypeBitmapFontData;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+
+@Mixin(NiceFreeTypeBitmapFontData.class)
+public class KatakanaJapaneseTextFixMixin {
+
+	@WrapMethod(method = "isChinese")
+	private static boolean checkIfKatakana(char c, Operation<Boolean> original) {
+		if(original.call(c) == true) {
+			return true; //already detected as a non-breaking character, return true
+		}
+		return c >= 0x30A0 && c <= 0x30FF; //if not detected as CJK, return true if katakana
+	}
+	
+}
